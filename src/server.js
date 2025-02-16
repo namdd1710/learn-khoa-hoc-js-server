0b66f8c3981b58b93148d7713040a60d3fa9d3a3
namdd1710 <donam17102002@gmail.com>
2025-02-16 09:35:51 +0700

    fix deploy

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+   "/api/*": "/$1",
+  })
+ );
 server.use(router)
 const port = process.env.PORT || 3000
 server.listen(port, () => {
